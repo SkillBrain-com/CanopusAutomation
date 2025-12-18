77a669168a6694d9d5b0dc1999505dd08d9bf0ef
Sandu Cristian Claudiu <sandu_cristian2007@yahoo.com>
2025-12-18 20:56:26 +0200

    testNG demo + logger + config file

---
+++ src/test/java/org/skillbrain/web/scripts/TestNgDemo.java
@@ -0,0 +1,13 @@
+package org.skillbrain.web.scripts;
+
+import org.testng.Assert;
+
+public class TestNgDemo {
+
+
+    public static void main(String[] args) {
+        Assert.assertEquals(10.1, 10.1);
+
+    }
+
+}
